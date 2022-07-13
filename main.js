3d87bec59f27c82cba83edaad8332490b94fa6f4
btramel <bradtramel@gmail.com>
2022-07-13 11:47:01 -0500

    Initializing repo

---
+++ main.js
@@ -0,0 +1,27 @@
+const hubble = document.querySelector('[data-hubble]')
+const webb = document.querySelector('[data-webb]')
+
+
+
+
+// // instantiate new juxtapose slider
+// const slider = new juxtapose.JXSlider('#juxtapose-wrapper',
+//     [
+//         {
+//             src: 'img/hubble/deep_field.png',
+//             label: '2004',
+//             credit: 'NASA, ESA, CSA, and STScI',
+//         },
+//         {
+//             src: 'img/hubble/deep_field(1).pn',
+//             label: '2022',
+//             credit: 'NASA, ESA, CSA, and STScI'
+//         }
+//     ],
+//     {
+//         animate: true,
+//         showLabels:true,
+//         showCredits: true,
+//         startingPostiion: '50%',
+//         makeResponsive: true
+//     });
